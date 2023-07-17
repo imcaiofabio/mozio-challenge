a567ffaf18630f0d6febc608e4d8926e247f7c66
imcaiofabio <caio.fabio91@gmail.com>
2023-07-17 00:21:49 -0300

    Changing DB engine

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mozio',
         'USER': 'postgres',
         'PASSWORD': 'caio123',
